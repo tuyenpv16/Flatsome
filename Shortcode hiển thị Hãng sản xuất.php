b520379c0d9d0bdffc5bd7bc651ee6351087fb9f
tuyenpv16 <32187306+tuyenpv16@users.noreply.github.com>
2024-04-26 14:32:30 +0700

    Create Shortcode hiển thị Hãng sản xuất.php

---
+++ Shortcode hiển thị Hãng sản xuất.php
@@ -0,0 +1,12 @@
+<?php
+// Tạo shortcode để hiển thị Hãng sản xuất và tuỳ chỉnh vị trí
+function display_manufacturer_shortcode() {
+    global $product;
+    $terms = get_the_term_list( $product->ID, 'manufacturer', '<span>Hãng sản xuất: ', ', ', '</span>' );
+    if ( $terms ) {
+        return $terms;
+    } else {
+        return 'Không có thông tin về hãng sản xuất.';
+    }
+}
+add_shortcode( 'manufacturer_info', 'display_manufacturer_shortcode' );
